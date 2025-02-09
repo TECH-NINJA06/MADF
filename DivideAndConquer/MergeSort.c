08b1d75ba16b84f2d263583a686c528f29cba6e9
TECH_NINJA06 <telangharsh06@gmail.com>
2025-02-09 22:16:04 +0530

    Rooh baba

--- DivideAndConquer/MergeSort.c
+++ DivideAndConquer/MergeSort.c
@@ -1,81 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#define MAX 100
+#define MAX_LEN 100
 
-void merge(int arr[], int temp[], int left, int mid, int right);
-void merge_sort(int arr[], int temp[], int left, int right);
+void merge(char arr[][MAX_LEN], char temp[][MAX_LEN], int left, int mid, int right);
+void merge_sort(char arr[][MAX_LEN], char temp[][MAX_LEN], int left, int right);
 
-void merge_sort(int arr[], int temp[], int left, int right) {
+void merge_sort(char arr[][MAX_LEN], char temp[][MAX_LEN], int left, int right) {
     if (left < right) {
         int mid = (left + right) / 2;
-
-        printf("Dividing: ");
-        for (int i = 0; i <= left - 1; i++) printf("%d ", arr[i]);
-        printf("[ ");
-        for (int i = left; i <= mid; i++) printf("%d ", arr[i]);
-        printf("| ");
-        for (int i = mid + 1; i <= right; i++) printf("%d ", arr[i]);
-        printf("] ");
-        for (int i = right + 1; i < MAX && arr[i] != 0; i++) printf("%d ", arr[i]);
-        printf("\n");
 
         merge_sort(arr, temp, left, mid);
         merge_sort(arr, temp, mid + 1, right);
 
         merge(arr, temp, left, mid, right);
-
-        // Print merging step
-        printf("Merging: ");
-        for (int i = 0; i <= left - 1; i++) printf("%d ", arr[i]);
-        printf("[ ");
-        for (int i = left; i <= right; i++) printf("%d ", arr[i]);
-        printf("] ");
-        for (int i = right + 1; i < MAX && arr[i] != 0; i++) printf("%d ", arr[i]);
-        printf("\n");
     }
 }
 
-void merge(int arr[], int temp[], int left, int mid, int right) {
+void merge(char arr[][MAX_LEN], char temp[][MAX_LEN], int left, int mid, int right) {
     int i = left, j = mid + 1, k = left;
 
     while (i <= mid && j <= right) {
-        if (arr[i] <= arr[j]) {
-            temp[k++] = arr[i++];
+        if (strcmp(arr[i], arr[j]) <= 0) {
+            strcpy(temp[k++], arr[i++]);
         } else {
-            temp[k++] = arr[j++];
+            strcpy(temp[k++], arr[j++]);
         }
     }
 
     while (i <= mid) {
-        temp[k++] = arr[i++];
+        strcpy(temp[k++], arr[i++]);
     }
     while (j <= right) {
-        temp[k++] = arr[j++];
+        strcpy(temp[k++], arr[j++]);
     }
 
     for (i = left; i <= right; i++) {
-        arr[i] = temp[i];
+        strcpy(arr[i], temp[i]);
     }
 }
-#define MAX 100
 
 int main() {
-    int arr[MAX], temp[MAX];
+    char arr[MAX][MAX_LEN], temp[MAX][MAX_LEN];
     int n;
 
-    printf("Enter number of elements: ");
+    printf("Enter number of strings: ");
     scanf("%d", &n);
 
-    printf("Enter elements: ");
+    printf("Enter strings: ");
     for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+        scanf("%s", arr[i]);
     }
 
     merge_sort(arr, temp, 0, n - 1);
 
-    printf("Sorted array: ");
+    printf("Sorted strings: ");
     for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+        printf("%s ", arr[i]);
     }
     printf("\n");
 
