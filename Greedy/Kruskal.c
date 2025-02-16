358a0e4c83dfffc114c058708460b42970051c34
Harsh Telang <telangharsh06@gmail.com>
2025-02-16 14:03:14 +0530

    Chen kuli ki Men Kuli

--- Greedy/Kruskal.c
+++ Greedy/Kruskal.c
@@ -1,157 +1,120 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define INF 2147483647
+#define MAX 100
 
-struct Edge
-{
-    int u, v, weight;
+struct Edge {
+    int startVertex, endVertex, weight;
 };
 
-struct Subset
-{
-    int parent;
-    int rank;
+struct MinHeap {
+    struct Edge edges[MAX];
+    int size;
 };
 
-int find(struct Subset subsets[], int i);
-void unionSets(struct Subset subsets[], int x, int y);
-void heapSort(struct Edge arr[], int n);
-void heapify(struct Edge arr[], int n, int i);
-void printParents(struct Subset subsets[], int n);
+int parent[MAX];
 
-void kruskal(int n, int m, struct Edge edges[])
-{
-    struct Subset subsets[n];
-    struct Edge result[n - 1];
-    int i, e = 0;
-    int mincost = 0;
-
-    heapSort(edges, m);
-
-    for (i = 0; i < n; i++)
-    {
-        subsets[i].parent = -2;
-        subsets[i].rank = 0;
-    }
-    printParents(subsets, n);
-    i = 0;
-    while (e < n - 1 && i < m)
-    {
-        struct Edge nextEdge = edges[i++];
-        int x = find(subsets, nextEdge.u);
-        int y = find(subsets, nextEdge.v);
-
-        if (x != y)
-        {
-            result[e++] = nextEdge;
-            mincost += nextEdge.weight;
-            unionSets(subsets, x, y);
-        }
-
-        printf("After considering edge (%d, %d):\n", nextEdge.u + 1, nextEdge.v + 1);
-        printParents(subsets, n);
-    }
-
-    if (e != n - 1)
-    {
-        printf("No Spanning Tree\n");
-        return;
-    }
-
-    printf("Edges in the Minimum Spanning Tree:\n");
-    for (i = 0; i < e; i++)
-    {
-        printf("(%d, %d) -> %d\n", result[i].u + 1, result[i].v + 1, result[i].weight);
-    }
-    printf("Total cost of the Minimum Spanning Tree: %d\n", mincost);
-}
-
-int find(struct Subset subsets[], int i)
-{
-    while (subsets[i].parent >= 0)
-    {
-        i = subsets[i].parent;
-    }
-    return i;
-}
-
-void unionSets(struct Subset subsets[], int x, int y)
-{
-    int rootX = find(subsets, x);
-    int rootY = find(subsets, y);
-
-    if (rootX != rootY)
-    {
-        if (subsets[rootY].rank < subsets[rootX].rank)
-        {
-            subsets[rootY].parent = rootX;
-        }
-        else if (subsets[rootY].rank > subsets[rootX].rank)
-        {
-            subsets[rootX].parent = rootY;
-        }
-        else
-        {
-            subsets[rootX].parent = rootY;
-            subsets[rootY].rank--;
-        }
+void heapify(struct MinHeap *heap, int index) {
+    int smallest = index;
+    int leftChild = 2 * index + 1;
+    int rightChild = 2 * index + 2;
+    if (leftChild < heap->size && heap->edges[leftChild].weight < heap->edges[smallest].weight)
+        smallest = leftChild;
+    if (rightChild < heap->size && heap->edges[rightChild].weight < heap->edges[smallest].weight)
+        smallest = rightChild;
+    if (smallest != index) {
+        struct Edge temp = heap->edges[index];
+        heap->edges[index] = heap->edges[smallest];
+        heap->edges[smallest] = temp;
+        heapify(heap, smallest);
     }
 }
 
-void heapSort(struct Edge arr[], int n)
-{
-    for (int i = n / 2 - 1; i >= 0; i--)
-    {
-        heapify(arr, n, i);
-    }
-    for (int i = n - 1; i > 0; i--)
-    {
-        struct Edge temp = arr[0];
-        arr[0] = arr[i];
-        arr[i] = temp;
-        heapify(arr, i, 0);
+struct Edge Delete(struct MinHeap *heap) {
+    struct Edge minEdge = heap->edges[0];
+    heap->edges[0] = heap->edges[--heap->size];
+    heapify(heap, 0);
+    return minEdge;
+}
+
+void buildHeap(struct MinHeap *heap) {
+    for (int i = heap->size / 2 - 1; i >= 0; i--)
+        heapify(heap, i);
+}
+
+int Find(int vertex) {
+    while(parent[vertex]>=0)
+        vertex = parent[vertex];
+    return vertex;
+}
+
+void Union(int vertex1, int vertex2) {
+    int root1 = Find(vertex1);
+    int root2 = Find(vertex2);
+    if (root1 != root2) {
+        parent[root1] = root2;
     }
 }
 
-void heapify(struct Edge arr[], int n, int i)
-{
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-    if (left < n && arr[left].weight > arr[largest].weight)
-    {
-        largest = left;
-    }
-    if (right < n && arr[right].weight > arr[largest].weight)
-    {
-        largest = right;
-    }
-    if (largest != i)
-    {
-        struct Edge temp = arr[i];
-        arr[i] = arr[largest];
-        arr[largest] = temp;
-        heapify(arr, n, largest);
-    }
-}
-
-void printParents(struct Subset subsets[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        printf("Vertex %d: Parent = %d\n", i + 1, subsets[i].parent + 1);
-    }
+void printParentArray(int numVertices) {
+    printf("Parent array: ");
+    for (int i = 0; i < numVertices; i++)
+        printf("%d ", parent[i]);
     printf("\n");
 }
 
-int main()
-{
-    int n = 8, m = 14;
-    struct Edge edges[] = {
-        {1, 4, 1}, {4, 5, 1}, {0, 2, 2}, {1, 3, 2}, {3, 5, 2}, {4, 7, 2}, {6, 7, 1}, {1, 2, 3}, {2, 7, 3}, {3, 4, 3}, {4, 6, 3}, {2, 4, 4}, {5, 6, 4}, {0, 1, 5}
-    };
+void kruskal(struct Edge edges[], int numVertices, int numEdges) {
+    struct MinHeap heap;
+    heap.size = numEdges;
+    for (int i = 0; i < numEdges; i++)
+        heap.edges[i] = edges[i];
+    
+    buildHeap(&heap);
+    
+    for (int i = 0; i < numVertices; i++)
+        parent[i] = -1;
+    
+    struct Edge result[MAX];
+    int minCost = 0, edgeCount = 0;
+    while (edgeCount < numVertices - 1 && heap.size > 0) {
+        struct Edge edge = Delete(&heap);
+        int startParent = Find(edge.startVertex);
+        int endParent = Find(edge.endVertex);
+        
+        if (startParent != endParent) {
+            result[edgeCount] = edge;
+            printf("Edge selected: %d - %d | Cost: %d\n", edge.startVertex, edge.endVertex, edge.weight);
+            minCost += edge.weight;
+            Union(startParent, endParent);
+            edgeCount++;
+            printf("MinCost=%d\n", minCost);
+            printParentArray(numVertices);
+            printf("\n");
+        }
+    }
+    if (edgeCount != numVertices - 1)
+        printf("No spanning tree exists\n");
+    else {
+        printf("Edges in the Minimum Spanning Tree:\n");
+        for (int i = 0; i < edgeCount; i++)
+            printf("%d - %d | Cost: %d\n", result[i].startVertex+1, result[i].endVertex+1, result[i].weight);
+        printf("Minimum Cost: %d\n", minCost);
+    }
+}
 
-    kruskal(n, m, edges);
+int main() {
+    int numVertices, numEdges;
+    printf("Enter number of vertices and edges: ");
+    scanf("%d %d", &numVertices, &numEdges);
+    
+    struct Edge edges[MAX];
+    printf("Enter edges (startVertex endVertex weight):\n");
+    for (int i = 0; i < numEdges; i++) {
+        scanf("%d %d %d", &edges[i].startVertex, &edges[i].endVertex, &edges[i].weight);
+        edges[i].startVertex--;
+        edges[i].endVertex--;  
+    }
+    
+    kruskal(edges, numVertices, numEdges);
     return 0;
 }
