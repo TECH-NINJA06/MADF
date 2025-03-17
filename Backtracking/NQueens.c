b82f362ca9ecfc8a7a2514aba511b20990fd43a6
Harsh Telang <telangharsh06@gmail.com>
2025-03-17 15:52:53 +0530

    KU LO SA

--- Backtracking/NQueens.c
+++ Backtracking/NQueens.c
@@ -1,68 +1,54 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
 
-#define N 5
+#define N 4
 
-void printSolution(int board[N][N]) {
+int x[N];
+
+int Place(int k, int i) {
+    for (int j = 0; j < k; j++) {
+        if (x[j] == i || abs(x[j] - i) == abs(j - k)) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void PrintBoard() {
+    char board[N][N];
+    for (int i = 0; i<N ; i++) {
+        for (int j = 0; j<N; j++) {
+            board[i][j] = '.';
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        board[i][x[i]] = 'Q';
+    }
+
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            printf(" %d ", board[i][j]);
+            printf("%c ", board[i][j]);
         }
         printf("\n");
     }
     printf("\n");
 }
 
-bool isSafe(int board[N][N], int row, int col) {
-    int i, j;
-
-    for (i = 0; i < col; i++)
-        if (board[row][i])
-            return false;
-
-    for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
-        if (board[i][j])
-            return false;
-
-    for (i = row, j = col; j >= 0 && i < N; i++, j--)
-        if (board[i][j])
-            return false;
-
-    return true;
-}
-
-bool solveNQUtil(int board[N][N], int col) {
-    if (col >= N) {
-        printSolution(board);
-        return true;
-    }
-
-    bool res = false;
-    for (int i = 0; i < N; i++) {
-        if (isSafe(board, i, col)) {
-            board[i][col] = 1;
-
-            res = solveNQUtil(board, col + 1) || res;
-
-            board[i][col] = 0;
+void NQueens(int k, int n) {
+    for (int i = 0; i < n; i++) {
+        if (Place(k, i)) {
+            x[k] = i;
+            if (k == n - 1) {
+                PrintBoard();
+            } else {
+                NQueens(k + 1, n);
+            }
         }
     }
-
-    return res;
-}
-
-bool solveNQ() {
-    int board[N][N] = { {0} };
-
-    if (!solveNQUtil(board, 0)) {
-        printf("Solution does not exist\n");
-        return false;
-    }
-
-    return true;
 }
 
 int main() {
-    solveNQ();
+    NQueens(0, N);
     return 0;
 }
