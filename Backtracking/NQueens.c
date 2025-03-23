63d75b8a18efd9089ed87d66a746f898facda087
D-Justin-Dsouza <justindsouza80@gmail.com>
2025-03-23 17:54:51 +0530

    Dance Moneky

--- Backtracking/NQueens.c
+++ Backtracking/NQueens.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int Place(int x[], int k, int i) {
+#define N 4
+
+int x[N];
+
+int Place(int k, int i) {
     for (int j = 0; j < k; j++) {
         if (x[j] == i || abs(x[j] - i) == abs(j - k)) {
             return 0;
@@ -10,7 +14,7 @@ int Place(int x[], int k, int i) {
     return 1;
 }
 
-void PrintBoard(int x[], int N) {
+void PrintBoard() {
     char board[N][N];
     for (int i = 0; i<N ; i++) {
         for (int j = 0; j<N; j++) {
@@ -31,7 +35,7 @@ void PrintBoard(int x[], int N) {
     printf("\n");
 }
 
-void NQueens(int x[], int k, int n) {
+void NQueens(int k, int n) {
     for (int i = 0; i < n; i++) {
         if (Place(k, i)) {
             x[k] = i;
@@ -45,10 +49,6 @@ void NQueens(int x[], int k, int n) {
 }
 
 int main() {
-    int N;
-    printf("Enter the number of queens: ");
-    scanf("%d", &N);
-    int x[N];
     NQueens(0, N);
     return 0;
 }
